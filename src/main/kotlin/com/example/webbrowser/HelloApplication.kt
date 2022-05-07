71e2d2fb44b4baff9ce3cf1c89ba7315ec895ae9
Cat-Man123 <65119790+Cat-Man123@users.noreply.github.com>
2022-05-07 11:47:23 +0200

    Update HelloApplication.kt

--- src/main/kotlin/com/example/webbrowser/HelloApplication.kt
+++ src/main/kotlin/com/example/webbrowser/HelloApplication.kt
@@ -2,17 +2,33 @@ package com.example.webbrowser
 
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.stage.Stage
 import javafx.stage.StageStyle
 
+
 class HelloApplication : Application() {
+
+    private var x = 0.0
+    private var y = 0.0
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load())
+        val fxmlLoader: Parent = FXMLLoader.load(HelloApplication::class.java.getResource("hello-view.fxml"))
+        val scene = Scene(fxmlLoader)
         stage.title = "WebBrowser"
         stage.initStyle(StageStyle.UNDECORATED)
         stage.scene = scene
+
+        fxmlLoader.setOnMousePressed { event ->
+            x = event.sceneX
+            y = event.sceneY
+        }
+
+        fxmlLoader.setOnMouseDragged { event ->
+            stage.x = event.sceneX - x
+            stage.y = event.sceneY - y
+        }
+
         stage.show()
     }
 }
